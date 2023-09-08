b6b9c8fd845cbee6cb0ee062072b8e949d0064bd
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-09-08 17:49:34 +0530

    C++, very_easy, returns num1 + num2, [1].

--- C++/very_easy/[1] Return the Sum of Two Numbers [VE].cpp
+++ C++/very_easy/[1] Return the Sum of Two Numbers [VE].cpp
@@ -26,7 +26,7 @@ ___
 
 
 
-[algebra] [language_fundamentals] [math] [numbers] 
+[algebra] [language_fundamentals] [math] [numbers]
 
 
 
@@ -59,5 +59,19 @@ This sounds like a weird link, but it might actually help you solve this challen
 _________
 
 */
-//Your code should go here:
+// Your code should go here:
 
+#include <iostream>
+using namespace std;
+int addition(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+int main()
+{
+    cout << addition(3, 2) << endl;
+    cout << addition(-3, -6) << endl;
+    cout << addition(7, 3) << endl;
+    return 0;
+}
