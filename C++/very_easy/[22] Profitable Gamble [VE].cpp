853d8873ed50d49caeb7d3065eac083753a0af1f
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-09-14 01:39:41 +0530

    C++, very_easy, returns true if profit in gamble, [22].

--- C++/very_easy/[22] Profitable Gamble [VE].cpp
+++ C++/very_easy/[22] Profitable Gamble [VE].cpp
@@ -27,7 +27,7 @@ _____
 A profitable gamble is a game that yields a positive net profit, where net profit is calculated in the following manner: net_outcome = probability_of_winning * prize - cost_of_playing.
 
 
-[conditions] [math] [validation] 
+[conditions] [math] [validation]
 
 
 
@@ -40,5 +40,24 @@ All variables use data-type during declaration to restrict the type of data to b
 _________
 
 */
-//Your code should go here:
+// Your code should go here:
 
+#include <iostream>
+
+bool profitableGamble(double prob, double prize, double pay)
+{
+    return prob * prize > pay;
+}
+
+int main()
+{
+    // Test cases.
+    std::cout << std::boolalpha << profitableGamble(0.2, 50, 9) << std::endl;
+    std::cout << std::boolalpha << profitableGamble(0.9, 1, 2) << std::endl;
+    std::cout << std::boolalpha << profitableGamble(0.9, 3, 2) << std::endl;
+
+    return 0;
+}
+
+// 14-09-2023
+// complete.
